34f4f6ab6f5797571d5307f4109baa992f6e5218
zacbailey <zbail25@wgu.ed>
2025-12-08 12:37:40 -0800

    Initial commit

---
+++ src/main/java/com/d288/zacbaileyd288/ZacBaileyD288Application.java
@@ -0,0 +1,13 @@
+package com.d288.zacbaileyd288;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZacBaileyD288Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZacBaileyD288Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/d288/zacbaileyd288/ZacBaileyD288ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d288.zacbaileyd288;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZacBaileyD288ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
